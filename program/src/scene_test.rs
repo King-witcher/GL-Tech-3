9563e8cc2e9b3010b3d9d5bc0f0004f8e1b9818b
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2025-09-12 19:04:49 -0300

    reorganize main

--- program/src/scene_test.rs
+++ program/src/scene_test.rs
@@ -12,19 +12,23 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file_system = file_system::load_file_system()?;
     let glass4a = file_system.get("textures/glass/marineglass4a.dds")?;
     let image = images::get_from_file(glass4a)?;
-
     let mut scene = Scene::new();
-    let texture: Texture = Texture::new(&image);
-    let plane = Plane::new(Vector(1.0, 0.0), Vector(1.0, 0.0), texture);
-    let mut plane: Entity = plane.into();
-    plane.add_script(Box::new(RotateScript));
-    scene.add_node(plane);
+
+    // Rotating plane
+    {
+        let texture = Texture::new(&image);
+        let primitive = Plane::new(Vector(0.0, 0.0), Vector(0.0, 1.0), texture);
+        let mut entity = Entity::from_plane(primitive);
+        entity.add_script(Box::new(RotateScript));
+        scene.add_node(entity);
+    }
+
     let renderer = RendererBuilder::new(scene)
         .width(1920)
         .height(1080)
         .fullscreen();
-    renderer.start();
 
+    renderer.start();
     Ok(())
 }
 
